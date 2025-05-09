51034452e27f3d14ebb749c027d0b0f9b838f9c0
han-jae-young <hanj921201@gmail.com>
2025-05-09 17:54:00 +0900

    eeprom 초기화 루틴 추가

---
+++ src/routine/RestartRoutine.cpp
@@ -0,0 +1,37 @@
+#include "RestartRoutine.h"
+#include "../storage/EEPROMStorage.h"
+
+RestartRoutine::RestartRoutine(uint8_t pin, unsigned long thresholdMillis)
+    : _pin(pin), _thresholdMillis(thresholdMillis), _lowStartTime(0), _wasLow(false)
+{
+    pinMode(_pin, INPUT);
+}
+
+void RestartRoutine::checkRoutine()
+{
+    int state = digitalRead(_pin);
+
+    if (state == LOW)
+    {
+        if (!_wasLow)
+        {
+            _lowStartTime = millis(); // Mark time when pin goes LOW
+            _wasLow = true;
+        }
+        else
+        {
+            if (millis() - _lowStartTime >= _thresholdMillis)
+            {
+                Serial.println("EEPROM data will be erased. Performing factory reset...");
+                EEPROMStorage::getInstance().reset(); // Erase EEPROM
+                delay(100);             // Ensure EEPROM is flushed
+                ESP.restart();          // Restart device
+                _wasLow = false;        // Reset flag (will not matter due to restart)
+            }
+        }
+    }
+    else
+    {
+        _wasLow = false; // Reset state if pin is HIGH
+    }
+}
---
+++ src/routine/RestartRoutine.h
@@ -0,0 +1,19 @@
+#ifndef RESTARTROUTINE_H
+#define RESTARTROUTINE_H
+
+#include <Arduino.h>
+
+class RestartRoutine
+{
+public:
+    RestartRoutine(uint8_t pin, unsigned long thresholdMillis);
+    void checkRoutine();
+
+private:
+    uint8_t _pin;
+    unsigned long _thresholdMillis;
+    unsigned long _lowStartTime;
+    bool _wasLow;
+};
+
+#endif
