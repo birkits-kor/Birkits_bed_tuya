71b748344e964ef80ed4a4c47dd353ea4c58fb82
han-jae-young <hanj921201@gmail.com>
2025-05-10 17:13:22 +0900

    update wifi manager

--- src/communication/WiFiManager.cpp
+++ src/communication/WiFiManager.cpp
@@ -2,15 +2,13 @@
 
 bool WiFiManager::connect()
 {
-
     delay(1000);
-    EEPROMStorage::getInstance().begin();
     if (!EEPROMStorage::getInstance().isDataPresent())
         return false;
 
     String ssid, password, deviceId, deviceSecret;
     EEPROMStorage::getInstance().loadCredentials(ssid, password, deviceId, deviceSecret);
-
+    Serial.printf("WIFI ssid:%s pw:%s\n", ssid.c_str(), password.c_str());
     WiFi.begin(ssid.c_str(), password.c_str());
     int attempts = 0;
     while (WiFi.status() != WL_CONNECTED && attempts < 20)
