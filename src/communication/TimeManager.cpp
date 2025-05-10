964933dc4301b102ed99807733a8e77e064da261
han-jae-young <hanj921201@gmail.com>
2025-05-10 17:12:52 +0900

    update time manager

--- src/communication/TimeManager.cpp
+++ src/communication/TimeManager.cpp
@@ -2,10 +2,26 @@
 #include <time.h>
 #include <WiFi.h>
 
-void TimeManager::sync() {
-    configTime(9 * 3600, 0, "pool.ntp.org", "time.nist.gov");
-    struct tm timeinfo;
-    while (!getLocalTime(&timeinfo)) {
+bool TimeManager::sync()
+{
+    configTime(gmt_offset_sec, daylight_offset_sec, ntp_server);
+    Serial.print("Waiting for NTP time sync: ");
+    while (time(nullptr) < 8 * 3600 * 2)
+    {
         delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("Time synchronized");
+    struct tm timeinfo;
+    if (getLocalTime(&timeinfo))
+    {
+        Serial.print("Current time: ");
+        Serial.println(asctime(&timeinfo));
+        return true;
+    }
+    else
+    {
+        Serial.println("Failed to obtain local time");
+        return false;
     }
 }
--- src/communication/TimeManager.h
+++ src/communication/TimeManager.h
@@ -1,9 +1,17 @@
 #ifndef TIME_MANAGER_H
 #define TIME_MANAGER_H
 
-class TimeManager {
+class TimeManager
+{
 public:
-    void sync();
+    bool sync();
+
+private:
+    const char *ntp_server = "pool.ntp.org"; // Default NTP server
+    const long gmt_offset_sec = 0;           // GMT offset in seconds (adjust for your time zone)
+    const int daylight_offset_sec = 0;       // Daylight saving time offset in seconds
+    const int maxRetries = 10;
+    int retryCount = 0;
 };
 
 #endif // TIME_MANAGER_H
