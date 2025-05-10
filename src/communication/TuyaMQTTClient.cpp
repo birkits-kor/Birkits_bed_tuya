974e54fd6f7c08dc008f50c401a0bd8b9e7d44c8
han-jae-young <hanj921201@gmail.com>
2025-05-10 17:13:12 +0900

    add tuya MQTT client

---
+++ src/communication/TuyaMQTTClient.cpp
@@ -0,0 +1,130 @@
+#include "TuyaMQTTClient.h"
+#include <ArduinoJson.h>
+#include <SHA256.h>
+#include <Base64.h>
+
+void TuyaMQTTClient::begin(WiFiClientSecure &secureClient, const char *broker, int port)
+{
+    client = &secureClient;
+    mqttClient.setClient(secureClient);
+    mqttClient.setServer(broker, port);
+    mqttClient.setCallback(mqttCallback);
+}
+
+bool TuyaMQTTClient::connect()
+{
+    if (mqttClient.connected())
+        return true;
+    if (!EEPROMStorage::getInstance().isDataPresent())
+        return false;
+
+    String a, b, deviceId, deviceSecret;
+    EEPROMStorage::getInstance().loadCredentials(a, b, deviceId, deviceSecret);
+    Serial.printf("TuyaMQTTClient id:%s pw:%s\n", deviceId.c_str(), deviceSecret.c_str());
+    client->setCACert(ca_cert);
+    const int maxAttempts = 10;
+    for (int attempt = 1; attempt <= maxAttempts; ++attempt)
+    {
+        if (!mqttClient.connected())
+        {
+            calcSignature(deviceId.c_str(), deviceSecret.c_str());
+            Serial.printf("Attempt %d: Connecting to MQTT Broker as %s...\n", attempt, clientID);
+
+            if (mqttClient.connect(clientID, username, password))
+            {
+                Serial.println("Connected to MQTT broker");
+
+                char auto_subscribe_topic[64];
+                sprintf(auto_subscribe_topic, "tylink/%s/channel/downlink/auto_subscribe", deviceId.c_str());
+                mqttClient.subscribe(auto_subscribe_topic);
+
+                sprintf(auto_subscribe_topic, mqtt_topic, deviceId.c_str());
+                mqttClient.publish(auto_subscribe_topic, "{\"data\":{\"format\":\"simple\"}}");
+                delay(100);
+                return true; // 연결 성공
+            }
+            else
+            {
+                Serial.print("Failed to connect to MQTT broker, rc=");
+                Serial.println(mqttClient.state());
+                delay(100); // 실패 시 대기 후 재시도
+            }
+        }
+        else
+        {
+            return true; // 이미 연결됨
+        }
+    }
+}
+
+void TuyaMQTTClient::loop()
+{
+    mqttClient.loop();
+}
+
+String TuyaMQTTClient::hmac256(const String &signcontent, const String &ds)
+{
+    byte hashCode[SHA256HMAC_SIZE];
+    SHA256 sha256;
+
+    const char *key = ds.c_str();
+    size_t keySize = ds.length();
+
+    sha256.resetHMAC(key, keySize);
+    sha256.update((const byte *)signcontent.c_str(), signcontent.length());
+    sha256.finalizeHMAC(key, keySize, hashCode, sizeof(hashCode));
+
+    String sign = "";
+    for (byte i = 0; i < SHA256HMAC_SIZE; ++i)
+    {
+        sprintf(password + 2 * i, "%02x", hashCode[i]);
+        sign += "0123456789ABCDEF"[hashCode[i] >> 4];
+        sign += "0123456789ABCDEF"[hashCode[i] & 0xf];
+    }
+
+    return sign;
+}
+
+int TuyaMQTTClient::calcSignature(const char *deviceId, const char *deviceSecret)
+{
+    if (NULL == deviceId || NULL == deviceSecret)
+    {
+        return -1;
+    }
+
+    uint32_t timestamp = time(nullptr);
+
+    /* client ID */
+    sprintf(username, "%s|signMethod=hmacSha256,timestamp=%d,secureMode=1,accessType=1", deviceId, timestamp);
+    Serial.print("username:");
+    Serial.println(username);
+
+    /* username */
+    sprintf(clientID, "tuyalink_%s", deviceId);
+    Serial.print("clientID:");
+    Serial.println(clientID);
+
+    /* password */
+    int i = 0;
+    char passward_stuff[255];
+    size_t slen = sprintf(passward_stuff, "deviceId=%s,timestamp=%d,secureMode=1,accessType=1", deviceId, timestamp);
+    hmac256(passward_stuff, deviceSecret);
+
+    Serial.print("password:");
+    Serial.println(password);
+
+    return 0;
+}
+
+void TuyaMQTTClient::mqttCallback(char *topic, byte *payload, unsigned int length)
+{
+    // 데이터 큐에 저장하는 방식으로 변경 필요요
+    Serial.print("Message received on topic: ");
+    Serial.print(topic);
+    Serial.print("]: ");
+    for (int i = 0; i < length; i++)
+    {
+        Serial.print((char)payload[i]);
+    }
+    Serial.println();
+}
