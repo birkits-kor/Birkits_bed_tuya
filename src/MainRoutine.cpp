25eb2de04abebe27f2654855cd80f867a492edaf
han-jae-young <hanj921201@gmail.com>
2025-06-25 23:22:28 +0900

    LED 인디케이터 task 추가

--- src/MainRoutine.cpp
+++ src/MainRoutine.cpp
@@ -1,6 +1,28 @@
 #include "MainRoutine.h"
 #include "communication/TimeManager.h"
 
+volatile bool wifiManager_result = false;
+volatile bool tuyaMQTTClient_result = false;
+
+void blinkTask(void *parameter)
+{
+    int val = 100;
+    while (true)
+    {
+        if(wifiManager_result)
+            val = 200;
+        if(tuyaMQTTClient_result)
+            val = 50; 
+        analogWrite(LED_BUILTIN, val);
+        vTaskDelay(50000 / val / portTICK_PERIOD_MS); // 500ms 대기
+        analogWrite(LED_BUILTIN, 0);
+        vTaskDelay(50000 / val / portTICK_PERIOD_MS);
+    }
+
+    // 혹시 루프에서 빠져나오면 태스크 삭제
+    vTaskDelete(NULL);
+}
+
 void MainRoutine::init()
 {
     Serial.begin(115200);
@@ -9,10 +31,16 @@ void MainRoutine::init()
     NVSStorage::getInstance().begin(); // 초기화 필수
     Serial.println("Birkits Bed start!!");
     configPortal.begin();
+    xTaskCreate(
+        blinkTask,   // 태스크 함수
+        "LED Blink", // 태스크 이름 (디버깅용)
+        1024,        // 스택 크기 (단위: 워드)
+        NULL,        // 파라미터
+        10,           // 우선순위 (0~25, 높을수록 우선)
+        NULL         // 태스크 핸들 (필요 없으면 NULL)
+    );
 
-    bool wifiManager_result = false;
     bool timeManager_result = false;
-    bool tuyaMQTTClient_result = false;
 
     if (wifiManager.connect())
     {
@@ -28,8 +56,6 @@ void MainRoutine::init()
     }
 
     Serial.printf("setup done wifiManager[%d] timeManager[%d] tuyaMQTTClient[%d]\n", wifiManager_result, timeManager_result, tuyaMQTTClient_result);
-    if (wifiManager_result && timeManager_result && tuyaMQTTClient_result)
-        digitalWrite(LED_BUILTIN, HIGH);
     controlRoutine.begin();
     conversionRoutine.begin();
     irDecoder.setup();
@@ -74,10 +100,7 @@ void MainRoutine::do100msTasks()
     {
         prev100ms = now;
         restartRoutine.checkRoutine();
-        if(tuyaMQTTClient.connect())
-            digitalWrite(LED_BUILTIN, HIGH);
-        else
-            digitalWrite(LED_BUILTIN, LOW);
+        tuyaMQTTClient_result = tuyaMQTTClient.connect();
         controlRoutine.loopByApp();
     }
 }
